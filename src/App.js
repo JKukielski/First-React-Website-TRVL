1bd68ece6f2cf0bc46013fb66196540cfbfba87f
Jasiu Kukielski <jankukielski1@gmail.com>
2022-06-05 14:41:25 +0200

    completed prototype website TRVL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
+
+  
 
 function App() {
   return (
@@ -10,7 +16,14 @@ function App() {
     <Router>
       <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route exact path="/" element=
+          {<Home/>} />
+          <Route exact path="/services" element=
+          {<Services/>} />
+          <Route exact path="/products" element=
+          {<Products/>} />
+          <Route exact path="/sign-up" element=
+          {<SignUp/>} />
         </Routes>
       </Router>
     </>
